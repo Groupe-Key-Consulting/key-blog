dad24c7a96db01cb418e3a86ab97a7cf93be542c
Gaëtan Bloch <gaetan.bloch@keyconsulting.fr>
2024-05-20 07:34:45 +0200

    chore(husky): init husky & install git-precommit-checks

---
+++ git-precommit-checks.config.mjs
@@ -0,0 +1,22 @@
+module.exports = {
+  display: {
+    notifications: true,
+    offendingContent: true,
+    rulesSummary: false,
+    shortStats: true,
+    verbose: false,
+  },
+  rules: [
+    {
+      message: 'You’ve got leftover conflict markers',
+      regex: /^[<>|=]{4,}/m,
+      nonBlocking: false,
+    },
+    {
+      filter: /\.([j|t]s)?(astro)?(mdx)?$/,
+      message: 'console.log() found',
+      nonBlocking: false,
+      regex: /console.log\(/,
+    },
+  ],
+};
