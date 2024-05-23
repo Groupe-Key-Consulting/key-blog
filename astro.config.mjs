9ee5ecdfd656b2673ff8bdb14fb43e6a307970e8
Gaëtan Bloch <gaetan.bloch@keyconsulting.fr>
2024-05-23 17:20:10 +0200

    style(tailwind): setup tailwind on astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()]
 });
