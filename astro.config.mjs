facc00dce6f2e880aec1e87820d4b4feb409cdbe
Gaëtan Bloch <gaetan.bloch@keyconsulting.fr>
2024-05-26 09:16:59 +0200

    chore(i18n): first step of setup i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  i18n: {
+    locales: ['es', 'fr'],
+    defaultLocale: 'es',
+  },
 });
